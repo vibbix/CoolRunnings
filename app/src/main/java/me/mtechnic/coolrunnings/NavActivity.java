f28308bf2a762a9d612d5ec3211dd6a552aecb44
Mark Beznos <mark.vibbix@gmail.com>
2017-03-30 21:56:43 -0400

    Added WiFi resolver Activity nav bar fixes

--- app/src/main/java/me/mtechnic/coolrunnings/NavActivity.java
+++ app/src/main/java/me/mtechnic/coolrunnings/NavActivity.java
@@ -4,12 +4,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class NavActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-    private TextView mTextMessage;
+public class NavActivity extends AppCompatActivity {
+    @BindView(R.id.message)
+    TextView mTextMessage;
+    @BindView(R.id.navigation)
+    BottomNavigationView navigation;
+    @BindView(R.id.toolbar)
+    android.support.v7.widget.Toolbar toolbar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -30,15 +38,35 @@ public class NavActivity extends AppCompatActivity {
             return false;
         }
     };
+    private android.support.v7.widget.Toolbar.OnMenuItemClickListener mOnMenuItemClickListListener =
+            new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.action_refresh:
+                            return true;
+                    }
+                    return false;
+                }
+            };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbartop, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        ButterKnife.bind(this);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.navigation);
+
+        toolbar.setOnMenuItemClickListener(mOnMenuItemClickListListener);
     }
 
 }
