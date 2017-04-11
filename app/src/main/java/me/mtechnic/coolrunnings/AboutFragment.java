b10ec09e091e7eb9f27aa7d80234753947aa69d0
Mark Beznos <mark.vibbix@gmail.com>
2017-04-10 23:22:57 -0400

    Clicking the github buttons opens our respective github pages

--- app/src/main/java/me/mtechnic/coolrunnings/AboutFragment.java
+++ app/src/main/java/me/mtechnic/coolrunnings/AboutFragment.java
@@ -1,6 +1,8 @@
 package me.mtechnic.coolrunnings;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -12,8 +14,10 @@ import android.widget.TextView;
 import com.flaviofaria.kenburnsview.KenBurnsView;
 import com.squareup.picasso.Picasso;
 
+import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * The fragment for the about view
@@ -26,14 +30,17 @@ public class AboutFragment extends Fragment {
     KenBurnsView sputnik;
     @BindView(R.id.versionno)
     TextView versionno;
-
+    @BindString(R.string.github_vibbix)
+    String githubVibbix;
+    @BindString(R.string.github_dechristopher)
+    String githubDechristopher;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedinstancestate) {
         View view = inflater.inflate(R.layout.fragment_about, container, false);
         ButterKnife.bind(this, view);
         Picasso.with(view.getContext())
                 .load(R.mipmap.sputnik)
                 .placeholder(R.mipmap.sputnik_nano)
-                .error(R.drawable.ic_error)
+                .error(R.mipmap.sputnik_nano)
                 .fit()
                 .into(this.sputnik);
         try {
@@ -42,6 +49,22 @@ public class AboutFragment extends Fragment {
             Log.d(TAG, "Failed to get package name");
         }
         return view;
+    }
+
+    @OnClick({R.id.btn_dechristopher, R.id.btn_vibbix})
+    public void githubOnClick(View view) {
+        Intent browserIntent = null;
+        switch (view.getId()) {
+            case R.id.btn_dechristopher:
+                browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(githubDechristopher));
+                break;
+            case R.id.btn_vibbix:
+                browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(githubVibbix));
+                break;
+        }
+        if (null != browserIntent) {
+            startActivity(browserIntent);
+        }
     }
 
 }
