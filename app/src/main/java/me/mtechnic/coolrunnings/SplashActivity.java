dde1b7587ec16132bbb169c89e4ac1e01e450b5d
Mark Beznos <mark.vibbix@gmail.com>
2017-04-10 20:43:04 -0400

    Made everything into fragments and managed to forget like all the javas

---
+++ app/src/main/java/me/mtechnic/coolrunnings/SplashActivity.java
@@ -0,0 +1,37 @@
+package me.mtechnic.coolrunnings;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Activity for splashscreen
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_LOCATION = 1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_LOCATION);
+            }
+        }
+        Intent intent = new Intent(this, NavActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
